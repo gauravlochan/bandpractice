2c7ab435a7736ebffcfa856763c63bfd9fb5a62e
Gaurav Lochan <gaurav@fastr.in>
2011-11-08 23:51:44 +0530

    fixed print

--- database.php
+++ database.php
@@ -2,7 +2,7 @@
 # This function reads your DATABASE_URL config var and returns a connection
 # string suitable for pg_connect. Put this in your app.
 function pg_connection_string_from_database_url() {
-  print ("<pre>" + $_ENV["DATABASE_URL"]);
+  print ($_ENV["DATABASE_URL"]);
   extract(parse_url($_ENV["DATABASE_URL"]));
   return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
 }
