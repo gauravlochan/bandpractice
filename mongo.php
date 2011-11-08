0cd2e00a66d36ebbfdf7fe08c1e8ae44b2f21a00
Gaurav Lochan <gaurav@fastr.in>
2011-11-09 00:36:01 +0530

    used sample doc from https://gist.github.com/1288447

--- mongo.php
+++ mongo.php
@@ -1,7 +1,23 @@
 <?php
+  # get the mongo db name out of the env
+  $mongo_url = parse_url(getenv("MONGO_URL"));
+  $dbname = str_replace("/", "", $mongo_url["path"]);
 
-$mongo_uri = getenv('MONGOLAB_URI');
-$m = new Mongo(); // connect
-$db = $m->selectDB("example");
+  # connect
+  $m   = new Mongo(getenv("MONGO_URL"));
+  $db  = $m->$dbname;
+  $col = $db->access;
 
+  # insert a document
+  $visit = array( "ip" => $_SERVER["HTTP_X_FORWARDED_FOR"] );
+  $col->insert($visit);
+
+  # print all existing documents
+  $data = $col->find();
+  foreach($data as $visit) {
+    echo "<li>" . $visit["ip"] . "</li>";
+  }
+
+  # disconnect
+  $m->close();
 ?>
