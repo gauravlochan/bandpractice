9a11cbe93a1391511c62377dad3dd0796b70e092
Gaurav Lochan <gaurav@fastr.in>
2011-11-10 11:47:20 +0530

    replaced with sdk example

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
-//FB sdk
+/**
+ * Copyright 2011 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
 require 'facebook.php';
 
 // Provides access to app specific values such as your app id and app secret.
@@ -33,16 +48,14 @@ if ($user) {
 // Login or logout url will be needed depending on current user state.
 if ($user) {
   $logoutUrl = $facebook->getLogoutUrl();
-  $friends = $facebook->api('/me/friends');
 } else {
   $loginUrl = $facebook->getLoginUrl();
 }
 
 // This call will always work since we are fetching public data.
 $naitik = $facebook->api('/naitik');
+
 ?>
-
-
 <!doctype html>
 <html xmlns:fb="http://www.facebook.com/2008/fbml">
   <head>
@@ -84,19 +97,6 @@ $naitik = $facebook->api('/naitik');
     <?php else: ?>
       <strong><em>You are not Connected.</em></strong>
     <?php endif ?>
-    
-    <?php
-    if ($user):
-      $data = $friends['data'];
-      echo count($data) . ' Friends';
-      echo '<hr />';
-      echo '<ul id="friends">';
-      foreach ($data as $key=>$value) {
-        echo '<li><img src="https://graph.facebook.com/' . $value['id'] . '/picture" title="' . $value['name'] . '"/></li>';
-      }
-      echo '</ul>';
-    endif
-    ?>
 
     <h3>Public profile of Naitik</h3>
     <img src="https://graph.facebook.com/naitik/picture">
